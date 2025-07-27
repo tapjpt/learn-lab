f3aea1536df4012479dd9c306d1c966361a2d7e8
tapjpt <tapjpt@outlook.com>
2025-07-27 19:54:14 -0400

    Update bond_math.py with calc_spot_rate and other functions

--- fixed_income_tools/bond_math.py
+++ fixed_income_tools/bond_math.py
@@ -62,26 +62,71 @@ def generate_cash_flows(coupon_rate, face_value, term, frequency):
     cf_dict = {}
     coupon_payment = (coupon_rate * face_value)/frequency
 
-    for n in range(n_period):
+    for n in range(1, n_period +1):
         # computation is done once, outside of the loop as it's only needed to be computed once & saves CPU
         cf = coupon_payment
         
         #to capture the final cash flow at maturity, interest  + face value
-        if n == n_period -1:
+        if n == n_period:
             cf += face_value
         cf_dict[n] = round(cf, 2)
     return cf_dict
 
 #--- DISCOUNTED CASH FLOW ---
 
+def bond_dcf(cf_dict, discount_rate, frequency):
+    """
+    Calculate the present value of a bond using DCF.
+
+    Parameters:
+    -----------
+    cash_flows : list of floats
+        Cash flows per period (e.g. from generate_cash_flows).
+    discount_rate : float
+        Periodic discount rate (e.g. 0.02 for 2% per period).
+    frequency : int
+        Number of periods per year (e.g. 2 for semiannual)
+
+    Returns:  
+        float
+        Present value of the bond.
+    """
+    #captures if discount rate is entered as a whole number. 
+    if discount_rate > 1:
+        discount_rate /= 100
+
+    r = discount_rate / frequency
+    return round(sum(cf/(1 + r)**t for t, cf in cf_dict.items()), 2)
 
 
 
 # --- SPOT RATE FUNCTIONS ---
-def calc_spot_rate(...):
-    """Compute the spot rate from zero-coupon bond price"""
+def calc_spot_rate(clean_price, term, face_value):
+    """
+    Compute the spot rate for a zero-coupon bond.
+    
+    Parameters:
+    -----------
+    clean_price : float
+        Price of the zero-coupon bond
+    term : int or float
+        Time to maturity (in years)
+    face_value : float
+        Par value of the bond (default = 1000)
 
-# --- BOOTSTRAPPIGN SPOT RATES ---
+    Returns:
+    --------
+    float
+        Spot rate as a decimal (e.g., 0.05 for 5%)
+    """
+    discount_factor = face_value / clean_price
+    spot_rate = discount_factor ** (1 / term) - 1
+    return round(spot_rate, 6)
+
+
+# --- BOOTSTRAPPING SPOT RATES ---
+def bootstrap_spot_rate(coupon, face_value, price, known_spots):
+
 
 # --- FORWARD RATE FUNCTIONS ---
 def forward_rate_1y1y(...):
